eef05291dc7627618a6beb93e0415478828f1f85
julianakahara668 <rm95317@estudante.fieb.edu.br>
2025-03-14 09:38:48 -0300

    Projeto2F

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Component } from "react";
+
+class App extends Component {
+
+  render  (){
+
+    return (
+      <div>
+        <h3> Aula React Vite</h3>
+        <p>Retomada de conteudo</p>
+      </div>
+    )
+  }
+}
+
+export defaultz
